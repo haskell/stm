6553e4541b597cfb78a832241346a73ae7c3189f
Ian Lynagh <igloo@earth.li>
2007-07-17 01:49:02 +0000

    Fix test conc053: Don't use pattern sigs gratuitously

--- tests/conc053.hs
+++ tests/conc053.hs
@@ -13,10 +13,11 @@ m = 3000  -- maximum delay
 
 main = do
    s <- newQSemN n
-   (is :: [Int]) <- sequence (take n (repeat (getStdRandom (randomR (1,m)))))
+   is <- sequence (take n (repeat (getStdRandom (randomR (1,m)))))
    mapM (fork_sleep s) is
    waitQSemN s n
    where
+	fork_sleep :: QSemN -> Int -> IO ThreadId
 	fork_sleep s i = forkIO (do waitQSemN s 1
 			  	    t <- registerDelay (i*1000)
 				    atomically $ (readTVar t >>= check)
