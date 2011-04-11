1b7720f73bd9c4ac48cd8a1b60b4f9ef97ba4544
Simon Marlow <marlowsd@gmail.com>
2011-04-11 11:06:24 +0100

    Control.Concurrent.STM.TVar: added modifyTVar, modifyTVar', swapTVar

--- Control/Concurrent/STM/TVar.hs
+++ Control/Concurrent/STM/TVar.hs
@@ -20,6 +20,9 @@ module Control.Concurrent.STM.TVar (
 	readTVar,
 	readTVarIO,
 	writeTVar,
+	modifyTVar,
+	modifyTVar',
+	swapTVar,
 #ifdef __GLASGOW_HASKELL__
 	registerDelay
 #endif
@@ -34,3 +37,32 @@ import Control.Sequential.STM
 #if ! (MIN_VERSION_base(4,2,0))
 readTVarIO = atomically . readTVar
 #endif
+
+
+-- Like 'modifyIORef' but for 'TVar'.
+-- | Mutate the contents of a 'TVar'. /N.B./, this version is
+-- non-strict.
+modifyTVar :: TVar a -> (a -> a) -> STM ()
+modifyTVar var f = do
+    x <- readTVar var
+    writeTVar var (f x)
+{-# INLINE modifyTVar #-}
+
+
+-- | Strict version of 'modifyTVar'.
+modifyTVar' :: TVar a -> (a -> a) -> STM ()
+modifyTVar' var f = do
+    x <- readTVar var
+    writeTVar var $! f x
+{-# INLINE modifyTVar' #-}
+
+
+-- Like 'swapTMVar' but for 'TVar'.
+-- | Swap the contents of a 'TVar' for a new value.
+swapTVar :: TVar a -> a -> STM a
+swapTVar var new = do
+    old <- readTVar var
+    writeTVar var new
+    return old
+{-# INLINE swapTVar #-}
+
