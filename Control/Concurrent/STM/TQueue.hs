bb97f631e615cfe1836d9c8f5071123f49442f32
David Feuer <David.Feuer@gmail.com>
2018-05-22 15:35:49 -0400

    Use a scheduled queue for TQueue
    Use a scheduled (real-time) queue for `TQueue`. This should
    fix Trac 9539 properly.

--- Control/Concurrent/STM/TQueue.hs
+++ Control/Concurrent/STM/TQueue.hs
@@ -24,9 +24,10 @@
 --  * it does /not/ provide equivalents of the 'dupTChan' and
 --    'cloneTChan' operations.
 --
--- The implementation is based on the traditional purely-functional
--- queue representation that uses two lists to obtain amortised /O(1)/
--- enqueue and dequeue operations.
+-- The implementation is based on a purely-functional queue representation that
+-- uses two lists and a schedule to obtain worst-case /O(1)/
+-- enqueue and dequeue operations. See Simple and Efficient Purely Functional
+-- Queues and Deques, by Chris Okasaki.
 --
 -- @since 2.4
 -----------------------------------------------------------------------------
@@ -53,19 +54,22 @@ import Data.Typeable (Typeable)
 -- | 'TQueue' is an abstract type representing an unbounded FIFO channel.
 --
 -- @since 2.4
-data TQueue a = TQueue {-# UNPACK #-} !(TVar [a])
-                       {-# UNPACK #-} !(TVar [a])
+data TQueue a =
+  TQueue {-# UNPACK #-} !(TVar [a]) -- front
+         {-# UNPACK #-} !(TVar [a]) -- rear
+         {-# UNPACK #-} !(TVar [a]) -- schedule
   deriving Typeable
 
 instance Eq (TQueue a) where
-  TQueue a _ == TQueue b _ = a == b
+  TQueue a _ _ == TQueue b _ _ = a == b
 
 -- |Build and returns a new instance of 'TQueue'
 newTQueue :: STM (TQueue a)
 newTQueue = do
   read  <- newTVar []
   write <- newTVar []
-  return (TQueue read write)
+  sched <- newTVar []
+  return (TQueue read write sched)
 
 -- |@IO@ version of 'newTQueue'.  This is useful for creating top-level
 -- 'TQueue's using 'System.IO.Unsafe.unsafePerformIO', because using
@@ -75,32 +79,49 @@ newTQueueIO :: IO (TQueue a)
 newTQueueIO = do
   read  <- newTVarIO []
   write <- newTVarIO []
-  return (TQueue read write)
+  sched <- newTVarIO []
+  return (TQueue read write sched)
 
 -- |Write a value to a 'TQueue'.
 writeTQueue :: TQueue a -> a -> STM ()
-writeTQueue (TQueue _read write) a = do
+writeTQueue (TQueue read write sched) a = do
   listend <- readTVar write
-  writeTVar write (a:listend)
+  sch <- readTVar sched
+  case sch of
+    _s:ch -> do
+      writeTVar write (a:listend)
+      writeTVar sched ch
+    [] -> do
+      front <- readTVar read
+      let front' = rotate front (a:listend) []
+      writeTVar read front'
+      writeTVar write []
+      writeTVar sched front'
+
+rotate :: [a] -> [a] -> [a] -> [a]
+rotate [] (r : _) as = r : as
+rotate (l : ls) (r : rs) as = l : rotate ls rs (r : as)
+rotate _ _ _ = error "TQueue queue invariant violated."
 
 -- |Read the next value from the 'TQueue'.
 readTQueue :: TQueue a -> STM a
-readTQueue (TQueue read write) = do
-  xs <- readTVar read
-  case xs of
-    (x:xs') -> do
-      writeTVar read xs'
+readTQueue (TQueue read write sched) = do
+  front <- readTVar read
+  case front of
+    [] -> retry
+    x:front' -> do
+      sch <- readTVar sched
+      case sch of
+        _s:ch -> do
+           writeTVar sched ch
+           writeTVar read front'
+        [] -> do
+           end <- readTVar write
+           let front'' = rotate front' end []
+           writeTVar read front''
+           writeTVar write []
+           writeTVar sched front''
       return x
-    [] -> do
-      ys <- readTVar write
-      case ys of
-        [] -> retry
-        _  -> do
-          let (z:zs) = reverse ys -- NB. lazy: we want the transaction to be
-                                  -- short, otherwise it will conflict
-          writeTVar write []
-          writeTVar read zs
-          return z
 
 -- | A version of 'readTQueue' which does not retry. Instead it
 -- returns @Nothing@ if no value is available.
@@ -112,45 +133,44 @@ tryReadTQueue c = fmap Just (readTQueue c) `orElse` return Nothing
 --
 -- @since 2.4.5
 flushTQueue :: TQueue a -> STM [a]
-flushTQueue (TQueue read write) = do
+flushTQueue (TQueue read write sched) = do
   xs <- readTVar read
   ys <- readTVar write
   unless (null xs) $ writeTVar read []
   unless (null ys) $ writeTVar write []
+  writeTVar sched []
   return (xs ++ reverse ys)
 
 -- | Get the next value from the @TQueue@ without removing it,
 -- retrying if the channel is empty.
 peekTQueue :: TQueue a -> STM a
-peekTQueue c = do
-  x <- readTQueue c
-  unGetTQueue c x
-  return x
+peekTQueue (TQueue read _write _sched) = do
+  front <- readTVar read
+  case front of
+    [] -> retry
+    x:_ -> return x
 
 -- | A version of 'peekTQueue' which does not retry. Instead it
 -- returns @Nothing@ if no value is available.
 tryPeekTQueue :: TQueue a -> STM (Maybe a)
-tryPeekTQueue c = do
-  m <- tryReadTQueue c
-  case m of
-    Nothing -> return Nothing
-    Just x  -> do
-      unGetTQueue c x
-      return m
+tryPeekTQueue (TQueue read _write _sched) = do
+  front <- readTVar read
+  case front of
+    [] -> return Nothing
+    x:_ -> return (Just x)
 
 -- |Put a data item back onto a channel, where it will be the next item read.
 unGetTQueue :: TQueue a -> a -> STM ()
-unGetTQueue (TQueue read _write) a = do
+unGetTQueue (TQueue read _write sched) a = do
   xs <- readTVar read
+  sch <- readTVar sched
   writeTVar read (a:xs)
+  writeTVar sched (a:sch)
 
 -- |Returns 'True' if the supplied 'TQueue' is empty.
 isEmptyTQueue :: TQueue a -> STM Bool
-isEmptyTQueue (TQueue read write) = do
+isEmptyTQueue (TQueue read _write _sched) = do
   xs <- readTVar read
   case xs of
     (_:_) -> return False
-    [] -> do ys <- readTVar write
-             case ys of
-               [] -> return True
-               _  -> return False
+    [] -> return True
